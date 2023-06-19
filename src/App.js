e3344bc53df259bc3ca8a57af704ec515f2e0beb
MAYANK CHETAN PARVATIA <104015658+mayank2368@users.noreply.github.com>
2023-06-19 19:59:22 +0200

    Added css

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import axios from "axios";
 import "./App.css";
 
 const Github = () => {
-  const [user, setUser] = useState([]);
-  const [result, setResult] = useState("");
+  const [user, setUser] = useState("");
+  const [result, setResult] = useState(null);
 
   const fetchData = async () => {
     try {
@@ -12,39 +12,51 @@ const Github = () => {
       setResult(res.data);
     } catch (err) {
       console.log(err);
-      setResult("");
+      setResult(null);
     }
   };
 
   const handleClick = () => {
-    setResult("");
+    setResult(null);
     fetchData();
   };
+
   return (
-    <>
-      <div className="App">Github Username Finder</div>
-      <label>
-        Username:
+    <div className="github-container">
+      <h1 className="github-heading">Github Username Finder</h1>
+
+      <div className="github-input">
+        <label htmlFor="username">Username:</label>
         <input
-          type="name"
+          id="username"
+          type="text"
           value={user}
           placeholder="Enter username here"
           onChange={(e) => setUser(e.target.value)}
         />
-      </label>
-      <button onClick={handleClick}>Search</button>
+        <button onClick={handleClick}>Search</button>
+      </div>
 
-      {result ? (
-        <div className="github-user">
-          Here it is...
-          <img src={result.avatar_url} alt="User Avatar" />
-          <h1>Name: {result.name}</h1>
-          <p>Repos: {result.public_repos}</p>
-        </div>
-      ) : (
-        <p>Enter username to find one...</p>
-      )}
-    </>
+      <div className="github-result">
+        {result ? (
+          <>
+            <div className="github-avatar">
+              <img src={result.avatar_url} alt="User Avatar" />
+            </div>
+            <div className="github-info">
+              <h2 className="github-name">User Name: {result.name}</h2>
+              <p className="github-repos">
+                Public Repositories: {result.public_repos}
+              </p>
+            </div>
+          </>
+        ) : (
+          <p className="github-placeholder">
+            Enter a username to find a user...
+          </p>
+        )}
+      </div>
+    </div>
   );
 };
 
